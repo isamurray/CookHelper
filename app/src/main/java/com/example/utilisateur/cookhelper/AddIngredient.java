d183fa5fd191266c0c8788e4c365594bae8db100
jobin <gchau069@uottawa.ca>
2016-11-30 15:47:12 -0500

    le button fait qqch

--- app/src/main/java/com/example/utilisateur/cookhelper/AddIngredient.java
+++ app/src/main/java/com/example/utilisateur/cookhelper/AddIngredient.java
@@ -2,12 +2,30 @@ package com.example.utilisateur.cookhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class AddIngredient extends AppCompatActivity {
+
+public class AddIngredient extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView ingredientname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingredient);
+        ingredientname = (TextView) findViewById(R.id.newIngredient);
+    }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()== R.id.addIngredientButton){
+             ingredientname.setText("Awignahan");
+        }
     }
 }
