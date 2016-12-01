dc73edabb28910afa921b57e9b85b3f58b01dc4e
cedc <cedricclement@me.com>
2016-12-01 13:55:44 -0500

    CHDBHandler working with basic CH classes such as Ingredient and Recipe. These classes have been 'dumbed down' in order to make initial work with DB easier. TODO: add ingredientList. WIP

--- app/src/main/java/com/example/utilisateur/cookhelper/AddIngredient.java
+++ app/src/main/java/com/example/utilisateur/cookhelper/AddIngredient.java
@@ -2,12 +2,29 @@ package com.example.utilisateur.cookhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class AddIngredient extends AppCompatActivity {
-
+    EditText ingredientBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ingredientBox = (EditText) findViewById(R.id.newIngredient);
         setContentView(R.layout.activity_add_ingredient);
+    }
+
+    public void updateFields(){
+        ingredientBox = (EditText) findViewById(R.id.newIngredient);
+    }
+
+    public void newIngredient(View view){
+        CHDBHandler handler = new CHDBHandler(this, null, null, 1);
+        updateFields();
+        String ingredientName = ingredientBox.getText().toString();
+        Ingredient ingredient = new Ingredient(ingredientName);
+
+        handler.addIngredient(ingredient);
     }
 }
--- app/src/main/java/com/example/utilisateur/cookhelper/AddRecipe.java
+++ app/src/main/java/com/example/utilisateur/cookhelper/AddRecipe.java
@@ -26,4 +26,6 @@ public class AddRecipe extends AppCompatActivity {
         });
     }
 
+
+
 }
