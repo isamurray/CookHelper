90674b28ea56b40f985f95963bea5e707f605eed
cedc <cedricclement@me.com>
2016-12-01 22:16:32 -0500

    DB now created, and properly initialised on appstart.

--- app/src/main/java/com/example/utilisateur/cookhelper/MainActivity.java
+++ app/src/main/java/com/example/utilisateur/cookhelper/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CHDBHandler db = new CHDBHandler(this,null,null,1);
+        db.getWritableDatabase();
+        db.close();
 
     }
     
