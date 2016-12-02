51638e3c969cef0275de6bce43f0567cc139cf1f
cedc <cedricclement@me.com>
2016-12-01 21:52:14 -0500

    Added debug menu for quick DB testing.

--- app/src/main/java/com/example/utilisateur/cookhelper/MainActivity.java
+++ app/src/main/java/com/example/utilisateur/cookhelper/MainActivity.java
@@ -15,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // getMenuInflater().inflate(R.menu.menu_menu_example, menu);
+        getMenuInflater().inflate(R.menu.debug, menu);
+        return true;
+    }
 
 
     public void onClickAddRecipePage(View v) {
@@ -41,6 +48,54 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplication(), AddInstructionsToRecipe.class);
         startActivityForResult(intent, 0);
     }
+    
+    // To respond to menu selections
+    // DEBUG
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        
+        if(item.getItemId() == R.id.menu_dbg_1){
+            System.out.println("\ndbg1\n");            
+        }else if(item.getItemId() == R.id.menu_dbg_2){
+            System.out.println("\ndbg2\n");            
+        }else if(item.getItemId() == R.id.menu_dbg_3){
+            System.out.println("\ndbg3\n");
+            dropAllTables();            
+        }else{
+            System.out.println("\nunknown\n");            
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Creates new ingredient entry in the database
+     */
+    public void dropAllTables(){
+        CHDBHandler handler = new CHDBHandler(this, null, null, 1);
+        handler.dropAllTables();
+        // System.out.println(recipes);
+        // updateFields();
+        // String ingredientName = ingredientBox.getText().toString();
+        // Ingredient ingredient = new Ingredient(ingredientName);
+
+        //handler.addIngredient(ingredient);
+    }    
+    
+    /**
+     * Creates new ingredient entry in the database
+     */
+    public void newIngredient(){
+        CHDBHandler handler = new CHDBHandler(this, null, null, 1);
+        Recipe[] recipes = handler.getAllRecipes();
+        System.out.println(recipes);
+        // updateFields();
+        // String ingredientName = ingredientBox.getText().toString();
+        // Ingredient ingredient = new Ingredient(ingredientName);
+
+        //handler.addIngredient(ingredient);
+    }
+
+
 
 
 }
