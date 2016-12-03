116d292ecf211e4fe084383164fecb9e0a8eddd7
minoucatou <minoucatou@gmail.com>
2016-12-02 21:28:50 -0500

    ajouter des activités pour le bouton Help

---
+++ app/src/main/java/com/example/utilisateur/cookhelper/HelpSearchRecipe.java
@@ -0,0 +1,15 @@
+package com.example.utilisateur.cookhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpSearchRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_search_recipe);
+
+
+    }
+}
